908678aa47bd0d1accc5e6f6f3dc67c748db53df
cjsquared-dev <camjohnson0618@gmail.com>
2024-11-11 16:22:44 -0700

    completed interface and updated Nav component

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,8 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+  readonly Name: string;
+  readonly Location: string;
+  readonly Email: string;
+  readonly Company: string;
+  readonly Bio: string;
+}
