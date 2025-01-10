50df8543efc43838b0534ea03c0ab2b042a3d063
cjsquared-dev <camjohnson0618@gmail.com>
2025-01-09 22:16:35 -0700

    updated port info

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
+
 dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: "./environment",
   plugins: [react()],
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  },
 });
