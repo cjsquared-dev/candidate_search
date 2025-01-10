9f1e41084833b16d764b8fc74a3acbd0b364bc5b
cjsquared-dev <camjohnson0618@gmail.com>
2025-01-09 22:20:57 -0700

    updated vite config

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   envDir: "./environment",
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Listen on all network interfaces
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
   },
 });
